df682390fb549282e45d8d1ff11c57010c06a605
Julien Delange <delange@amazon.com>
2017-10-13 15:35:28 -0700

    Add results for ex5

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -27,22 +27,15 @@ error_val = zeros(length(lambda_vec), 1);
 %
 % Note: You can loop over lambda_vec with the following:
 %
-%       for i = 1:length(lambda_vec)
-%           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
-%           % regression with regularization parameter lambda
-%           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
-%       end
-%
-%
-
-
-
-
-
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    % very similar to what we already did with learningCurve.m
+    theta = trainLinearReg(X, y, lambda);
+    [train, garbage] = linearRegCostFunction(X,y,theta,0);
+    [val, garbage] = linearRegCostFunction(Xval,yval,theta,0);
+    error_train(i) = train;
+    error_val(i) = val;
+end
 
 
 
